540aa21466ebc400204a3cf326e88b0d88f3906c
Simon Kim <mrsimonkim@gmail.com>
2021-09-13 12:42:48 -0700

    Simon-2

---
+++ main.js
@@ -0,0 +1,4 @@
+var $addEntryButton = document.querySelector('.add-entry-button');
+$addEntryButton.addEventListener('click', function (event) {
+  // var addEntryModal = document.querySelector('.add-entry-button-container');//
+});
